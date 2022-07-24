069c2c1a1f7929171a6a70e5c61a687badc6fe94
Olena Sh <h.shinkarenko@gmail.com>
2022-07-24 13:01:06 +0300

    Added Calculation

---
+++ Java/Calculaton/src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    public static void main(String[] args) {
+        // boolean a = true;
+        // boolean y = true;
+        //  boolean z = false;
+        // System.out.println(x^y);
+         /* if (a<3]a>2) {
+            System.out.println("a<3");
+        } if (a>4) {
+            System.out.println("a>4");
+        }*/
+         /* if (a>3) {
+            System.out.println("a>3");
+        } else {
+            System.out.println("a>4");
+       */
+        int a = 14;
+        int b = 5;
+
+        System.out.println(a > 3 ? "true" : "false");
+    }
+}
+    //booleanExpression ? Expression_1_(if true) : expression_2_(if false)
+
