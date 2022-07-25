ed75c687ceb59b89c218eb40da0ff7d3b86812e2
Olena Sh <h.shinkarenko@gmail.com>
2022-07-25 20:50:59 +0300

    Changes made. (except "to")

--- Java/Task1/src/PersonInformation.java
+++ Java/Task1/src/PersonInformation.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
+        import java.util.Scanner;
+        //PersonInformation class declaration
+        public class PersonInformation {
+        public  static void  main(String[] args) {
 
-    //PersonInformation class declaration
-    public class PersonInformation {
-    public  static void  main(String[] args) {
-
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Hello. What is your name?");
-    String name = sc.nextLine();
-    System.out.println("Where are you live  " + name + "?");
-    String adress = sc.nextLine();
-    System.out.println(adress);
-    System.out.println( name + "; " + adress);
-    System.out.println("Nice to meet you " + name + " from " + adress);
-}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello. What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Where are you live, " + name + "?");
+        String adress = sc.nextLine();
+        System.out.println(adress);
+        System.out.println(name + " " + adress);
+        System.out.println("Nice to meet you " + name + " from " + adress + ".");
+    }
 }
